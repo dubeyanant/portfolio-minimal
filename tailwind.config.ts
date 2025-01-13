86948d2dd0d07914d02d59c3bae8c05e50868717
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2025-01-13 21:07:46 +0530

    feat: add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
 	darkMode: ["class"],
@@ -55,6 +56,11 @@ export default {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
 				sm: "calc(var(--radius) - 4px)",
+			},
+			fontFamily: {
+				sans: ["var(--font-sans)", ...fontFamily.sans],
+				serif: ["var(--font-serif)", ...fontFamily.serif],
+				mono: ["var(--font-mono)", ...fontFamily.mono],
 			},
 		},
 	},
